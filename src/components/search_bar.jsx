b946f45b660cfc8598b2bdbcc075d2a3de6e13c7
Eric Pan <epan@users.noreply.github.com>
2017-05-01 15:04:44 -0700

    (feat) Log search results to console from YT

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
   render () {
-    return <input />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })}
+        />
+      </div>
+    );
+
   }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YOUTUBE_API_KEY from '../env/youtube-api-key';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+
+YTSearch({key: YOUTUBE_API_KEY, term: 'surfing'}, data => console.log(data));
 
 const App = () => (
   <div>
